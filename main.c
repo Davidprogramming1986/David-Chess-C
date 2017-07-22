6fb35bee67037fb946f85a44c49f01884a3d344e
Davidprogramming1986 <davidholder@davids-imac.home>
2017-07-22 22:03:02 +0100

    part 13

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include "stdlib.h"
 #include "defs.h"
 #include "init.c"
 #include "bitboards.c"
@@ -7,31 +8,8 @@
 int main() {
 
   AllInit();
-
-  U64 playBitBoard = 0ULL;
-
-  printf("start:\n\n");
-  PrintBitBoard(playBitBoard);
-
-  playBitBoard |= (1ULL << SQ64(D2));
-  printf("D2 Added: \n\n");
-  PrintBitBoard(playBitBoard);
-
-  playBitBoard |= (1ULL << SQ64(G5));
-  printf("G5 Added: \n\n");
-  PrintBitBoard(playBitBoard);
-
-  printf("\n\n");
-
-  int count = CNT(playBitBoard);
-
-  printf("Count:%d\n\n", count);
-
-  int index = POP(&playBitBoard);
-  printf("index:%d\n", index);
-  PrintBitBoard(playBitBoard);
-  count = CNT(playBitBoard);
-  printf("Count:%d\n\n", count);
+  int index = 0;
+  
 
   return 0;
 }
